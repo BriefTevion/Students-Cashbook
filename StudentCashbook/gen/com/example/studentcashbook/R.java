44732c92c82e3fddd861f85a38b3597e432516a0
brieftevion <Flo@192.168.169.3>
2014-03-01 16:03:57 +0100

    Navigation Drawer created

--- StudentCashbook/gen/com/example/studentcashbook/R.java
+++ StudentCashbook/gen/com/example/studentcashbook/R.java
@@ -8,6 +8,9 @@
 package com.example.studentcashbook;
 
 public final class R {
+    public static final class array {
+        public static final int string_array_navigation_drawer=0x7f050000;
+    }
     public static final class attr {
     }
     public static final class dimen {
@@ -21,34 +24,38 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
-        public static final int rectangle_orange=0x7f020001;
-        public static final int roundedbutton=0x7f020002;
-        public static final int roundededittext=0x7f020003;
+        public static final int ic_drawer=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int rectangle_orange=0x7f020002;
+        public static final int roundedbutton=0x7f020003;
+        public static final int roundededittext=0x7f020004;
     }
     public static final class id {
-        public static final int Anmeldung=0x7f08000a;
-        public static final int Anmerkung=0x7f080000;
-        public static final int Benutzername=0x7f08000b;
-        public static final int Betrag=0x7f080002;
-        public static final int Datum=0x7f080004;
-        public static final int EinnahmeBestaetigen=0x7f080001;
-        public static final int KategorieSpinner=0x7f080003;
-        public static final int Passwort=0x7f08000d;
-        public static final int Pswdzuruecksetzen=0x7f08000f;
-        public static final int RectangleOra=0x7f080006;
-        public static final int Uhrzeit=0x7f080005;
-        public static final int Willkommen=0x7f080008;
-        public static final int action_settings=0x7f080015;
-        public static final int anlegen=0x7f080012;
-        public static final int imageView1=0x7f080007;
-        public static final int neuerBenutzer=0x7f08000e;
-        public static final int neuerBenutzername=0x7f080011;
-        public static final int neuerBenutzernameEingabe=0x7f080010;
-        public static final int neuesPasswort=0x7f080014;
-        public static final int neuesPasswortEingabe=0x7f080013;
-        public static final int textView1=0x7f080009;
-        public static final int textView2=0x7f08000c;
+        public static final int Anmeldung=0x7f09000a;
+        public static final int Anmerkung=0x7f090000;
+        public static final int Benutzername=0x7f09000b;
+        public static final int Betrag=0x7f090002;
+        public static final int Datum=0x7f090004;
+        public static final int EinnahmeBestaetigen=0x7f090001;
+        public static final int KategorieSpinner=0x7f090003;
+        public static final int Passwort=0x7f09000d;
+        public static final int Pswdzuruecksetzen=0x7f09000f;
+        public static final int RectangleOra=0x7f090006;
+        public static final int Uhrzeit=0x7f090005;
+        public static final int Willkommen=0x7f090008;
+        public static final int action_settings=0x7f090018;
+        public static final int anlegen=0x7f090015;
+        public static final int content_frame=0x7f090011;
+        public static final int drawer_layout=0x7f090010;
+        public static final int imageView1=0x7f090007;
+        public static final int left_drawer=0x7f090012;
+        public static final int neuerBenutzer=0x7f09000e;
+        public static final int neuerBenutzername=0x7f090014;
+        public static final int neuerBenutzernameEingabe=0x7f090013;
+        public static final int neuesPasswort=0x7f090017;
+        public static final int neuesPasswortEingabe=0x7f090016;
+        public static final int textView1=0x7f090009;
+        public static final int textView2=0x7f09000c;
     }
     public static final class layout {
         public static final int activity_ausgabe=0x7f030000;
@@ -62,47 +69,50 @@ public final class R {
         public static final int activity_neuer_nutzer=0x7f030008;
         public static final int activity_neues_sparziel=0x7f030009;
         public static final int activity_sparziel=0x7f03000a;
+        public static final int drawer_listview_item=0x7f03000b;
     }
     public static final class menu {
-        public static final int ausgabe=0x7f070000;
-        public static final int einnahme=0x7f070001;
-        public static final int einstellungen=0x7f070002;
-        public static final int kategorien=0x7f070003;
-        public static final int login=0x7f070004;
-        public static final int main=0x7f070005;
-        public static final int monat_ausgabe=0x7f070006;
-        public static final int monat_einnahme=0x7f070007;
-        public static final int neuer_nutzer=0x7f070008;
-        public static final int neues_sparziel=0x7f070009;
-        public static final int sparziel=0x7f07000a;
+        public static final int ausgabe=0x7f080000;
+        public static final int einnahme=0x7f080001;
+        public static final int einstellungen=0x7f080002;
+        public static final int kategorien=0x7f080003;
+        public static final int login=0x7f080004;
+        public static final int main=0x7f080005;
+        public static final int monat_ausgabe=0x7f080006;
+        public static final int monat_einnahme=0x7f080007;
+        public static final int neuer_nutzer=0x7f080008;
+        public static final int neues_sparziel=0x7f080009;
+        public static final int sparziel=0x7f08000a;
     }
     public static final class string {
-        public static final int Anlegen=0x7f050018;
-        public static final int Anmelden=0x7f050010;
-        public static final int Anmerkung=0x7f050011;
-        public static final int BenutzerAnlegen=0x7f050017;
-        public static final int Benutzername=0x7f05000d;
-        public static final int Datum=0x7f050012;
-        public static final int Logo=0x7f05000e;
-        public static final int NeuerBenutzer=0x7f050015;
-        public static final int OrangeRectangle=0x7f050014;
-        public static final int Passwort=0x7f05000f;
-        public static final int PswdZurueck=0x7f050019;
-        public static final int Uhrzeit=0x7f050013;
-        public static final int Willkommen=0x7f05000c;
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
-        public static final int title_activity_ausgabe=0x7f050005;
-        public static final int title_activity_einnahme=0x7f050004;
-        public static final int title_activity_einstellungen=0x7f05000b;
-        public static final int title_activity_kategorien=0x7f050006;
-        public static final int title_activity_login=0x7f050003;
-        public static final int title_activity_monat_ausgabe=0x7f050008;
-        public static final int title_activity_monat_einnahme=0x7f050007;
-        public static final int title_activity_neuer_nutzer=0x7f050016;
-        public static final int title_activity_neues_sparziel=0x7f05000a;
-        public static final int title_activity_sparziel=0x7f050009;
+        public static final int Anlegen=0x7f06001a;
+        public static final int Anmelden=0x7f060012;
+        public static final int Anmerkung=0x7f060013;
+        public static final int BenutzerAnlegen=0x7f060019;
+        public static final int Benutzername=0x7f06000f;
+        public static final int Datum=0x7f060014;
+        public static final int Logo=0x7f060010;
+        public static final int NeuerBenutzer=0x7f060017;
+        public static final int OrangeRectangle=0x7f060016;
+        public static final int Passwort=0x7f060011;
+        public static final int PswdZurueck=0x7f06001b;
+        public static final int Uhrzeit=0x7f060015;
+        public static final int Willkommen=0x7f06000e;
+        public static final int action_settings=0x7f060003;
+        public static final int app_name=0x7f060002;
+        public static final int drawer_close=0x7f060001;
+        public static final int drawer_open=0x7f060000;
+        public static final int hello_world=0x7f060004;
+        public static final int title_activity_ausgabe=0x7f060007;
+        public static final int title_activity_einnahme=0x7f060006;
+        public static final int title_activity_einstellungen=0x7f06000d;
+        public static final int title_activity_kategorien=0x7f060008;
+        public static final int title_activity_login=0x7f060005;
+        public static final int title_activity_monat_ausgabe=0x7f06000a;
+        public static final int title_activity_monat_einnahme=0x7f060009;
+        public static final int title_activity_neuer_nutzer=0x7f060018;
+        public static final int title_activity_neues_sparziel=0x7f06000c;
+        public static final int title_activity_sparziel=0x7f06000b;
     }
     public static final class style {
         /** 
@@ -126,10 +136,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
--- StudentCashbook/src/com/example/studentcashbook/MainActivity.java
+++ StudentCashbook/src/com/example/studentcashbook/MainActivity.java
@@ -1,20 +1,81 @@
 package com.example.studentcashbook;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.content.Intent;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.widget.DrawerLayout;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends Activity {
 
+	private String[] activitiesList;
+	private DrawerLayout dl;
+	private ListView dLv;
+	private ActionBarDrawerToggle abdt;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
+		//Drawer List initialisieren
+		activitiesList = getResources().getStringArray(R.array.string_array_navigation_drawer);
+		dl = (DrawerLayout) findViewById(R.id.drawer_layout);
+		dLv = (ListView) findViewById(R.id.left_drawer);
+		
+		//Adapter erzeugen für die ListView
+		dLv.setAdapter(new ArrayAdapter<String>(this, R.layout.drawer_listview_item, activitiesList));
+
+		//Icon
+		dl = (DrawerLayout) findViewById(R.id.drawer_layout);
+		
+		//Konfigurieren des Drawer Icons
+		abdt = new ActionBarDrawerToggle(this, dl, R.drawable.ic_drawer, R.string.drawer_open, R.string.drawer_close)
+		{	
+		};
+		
+		//ActionBarDrawerToggle als Drawer Listener setzen
+		dl.setDrawerListener(abdt);
+		
+		//Pfeil-Icon setzen
+		getActionBar().setDisplayHomeAsUpEnabled(true);
+		getActionBar().setHomeButtonEnabled(true);
+		
 		
 	}
 
+	//Konfiguration wird übergeben
+	@Override
+	public void onConfigurationChanged(Configuration newConfig) {
+		super.onConfigurationChanged(newConfig);
+		abdt.onConfigurationChanged(newConfig);
+	}
+	
+	//Aufruf bei Klick auf Item
+	@Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+		// Aufruf von abdt.onOptionsItemSelected()
+        // Wenn true, dann Click Event behandelt
+        if (abdt.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+	}
+	
+	@Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        abdt.syncState();
+    }
+	
+	
+	
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
@@ -22,4 +83,6 @@ public class MainActivity extends Activity {
 		return true;
 	}
 
+	
+	
 }
